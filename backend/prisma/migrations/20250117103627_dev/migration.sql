807ba616081f44a73c7248af80de44116b57c229
aryan-79 <aryanmgr79@gmail.com>
2025-01-17 17:57:08 +0545

    new migration and added env example

--- backend/prisma/migrations/20250117103627_dev/migration.sql
+++ backend/prisma/migrations/20250117103627_dev/migration.sql
@@ -1,9 +1,15 @@
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('IN_PROGRESS', 'COMPLETED', 'OVERDUE');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "refreshToken" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -11,11 +17,13 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Task" (
     "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "dueDate" TEXT NOT NULL,
-    "status" INTEGER NOT NULL,
+    "status" "TaskStatus" NOT NULL,
     "user_id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
 );
